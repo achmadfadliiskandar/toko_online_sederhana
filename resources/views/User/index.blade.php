708f982088405ec97fe3fc2b94a75e7b83aaba9b
achmadfadliiskandar <af137357@gmail.com>
2021-07-11 21:46:41 +0700

    silahkan di download dan dicustom

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -6,7 +6,7 @@
 @section('container')
 <div class="card">
     <div class="card-header">
-        <h3 class="card-title">DataTable with default features</h3>
+        <h3 class="card-title">Data user</h3>
     </div>
     <!-- /.card-header -->
     <div class="card-body">
